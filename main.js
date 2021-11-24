c646a4d7512e9c395062a5f1b5e254710325f4b5
Joni Korpi <me@jonikorpi.com>
2021-11-24 14:22:04 +0200

    Fix child ordering bugs, escalate reruns

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ let later;
 const updateQueue = new Set();
 const processQueue = () => {
   for (const context of updateQueue) {
-    updateState(context, context.nextValue);
+    updateState(context, context.body.nextValue);
   }
   later = null;
 };
 const updateState = (context, newValue) => {
+  console.log("================ setting", newValue);
   context.body[0] = newValue;
   rerun(context.parent, true);
 };
@@ -19,26 +20,37 @@ const createContext = (body, type) => ({
   cleanups: new Set(),
   value: null,
   shouldUpdate: true,
+  hasReturned: false,
   type,
   body,
 });
 
 const rootContext = createContext(() => {
+  const { shouldUpdate } = rootContext;
   for (const child of rootContext.children) {
-    rerun(child);
+    rerun(child, shouldUpdate);
   }
-}, "root");
+}, "rootContext");
 const stateType = "state";
-let effectTypeCounter = 1;
+let effectTypeCounter = 0;
 const indexStack = [-1];
 const stack = [rootContext];
 
-export const rerun = (context, forceUpdate = false) => {
-  stack.push(context.parent);
+export const rerun = (context, shouldUpdate = false) => {
+  let contextToRerun = context;
+  contextToRerun.shouldUpdate = shouldUpdate;
+
+  console.log("starting a rerun of", context.type);
+
+  while (contextToRerun.hasReturned && contextToRerun.parent !== rootContext) {
+    contextToRerun = contextToRerun.parent;
+    console.log("escalating rerun up to", contextToRerun.type);
+    contextToRerun.shouldUpdate = shouldUpdate;
+  }
+
+  stack.push(contextToRerun.parent);
   indexStack.push(-1);
-  context.shouldUpdate = forceUpdate;
-  context.body.call(null, ...context.argumentCache.values());
-  context.shouldUpdate = false;
+  contextToRerun.body.call(null, ...context.argumentCache.values());
   stack.pop();
   indexStack.pop();
 };
@@ -61,11 +73,14 @@ const getContext = (type) => {
     context = currentChild;
   } else {
     // Try to find the next matching child
-    for (let index = currentIndex, { length } = children; index < length; index++) {
+    for (let index = currentIndex + 1, { length } = children; index < length; index++) {
       const child = children[index];
       if (child.type === type) {
         console.log("found later at", index);
         context = child;
+        // Move it into its new place
+        children.splice(index, 1);
+        children.splice(currentIndex, 0, child);
         break;
       }
     }
@@ -92,15 +107,13 @@ export const state = (defaultInitialValue, getSetter, compare = defaultCompare) 
 
       const get = () => body[0];
       const set = (newValue) => {
-        console.log("===================================== trying to set", newValue);
         if (!compare(body[0], newValue)) {
           if (stack.length > 1) {
-            console.log("setting later", newValue);
+            console.log("========================= setting later", newValue);
             updateQueue.add(context);
             body.nextValue = newValue;
             later = later || schedule(processQueue);
           } else {
-            console.log("setting now", newValue);
             updateState(context, newValue);
           }
         }
@@ -121,33 +134,30 @@ export const state = (defaultInitialValue, getSetter, compare = defaultCompare) 
   return body;
 };
 
-export const effect = (thing, compare = defaultCompare) => {
-  const type = effectTypeCounter++;
+export const effect = (thing, compare = defaultCompare, name = "") => {
+  const type = `${name} (${effectTypeCounter++})`;
 
   const body = function () {
     let context = getContext(type);
-    let shouldUpdate = false;
 
     if (!context) {
       console.log("create", type);
       context = createContext(body, type);
       addContext(context);
-      shouldUpdate = true;
     }
 
     const { argumentCache } = context;
-    shouldUpdate = shouldUpdate || context.shouldUpdate;
 
     for (let index = 0; index < Math.max(arguments.length, argumentCache.size); index++) {
       const argument = argumentCache.get(index);
       const newArgument = arguments[index];
       if (!compare(argument, newArgument, index)) {
         argumentCache.set(index, newArgument);
-        shouldUpdate = true;
+        context.shouldUpdate = true;
       }
     }
 
-    if (shouldUpdate) {
+    if (context.shouldUpdate) {
       stack.push(context);
       indexStack.push(-1);
 
@@ -158,15 +168,21 @@ export const effect = (thing, compare = defaultCompare) => {
       // console.error(error);
       // }
 
+      if (!context.hasReturned) context.hasReturned = context.value !== undefined;
+      context.shouldUpdate = false;
+
       // Destroy children that were not visited on this execution
       const { children } = context;
       const { length } = children;
-      const nextIndex = Math.max(indexStack[indexStack.length - 1], 0);
-      for (let index = nextIndex; index < length - 1; index++) {
-        console.log("destroying leftover child", index, length);
-        destroy(children[index]);
+      const nextIndex = indexStack[indexStack.length - 1] + 1;
+
+      if (nextIndex < length) {
+        console.log(nextIndex, children);
+        for (let index = nextIndex; index < length; index++) {
+          destroy(children[index]);
+        }
+        children.splice(nextIndex);
       }
-      children.splice(nextIndex + 1);
 
       stack.pop();
       indexStack.pop();
@@ -179,8 +195,10 @@ export const effect = (thing, compare = defaultCompare) => {
 };
 
 const destroy = (context) => {
+  console.log("destroying", context.type);
   runCleanup(context, true);
   context.parent = null;
+  context.value = null;
   context.argumentCache.clear();
 
   for (const child of context.children) {
@@ -211,29 +229,7 @@ export const html = new Proxy(
       const cached = htmlCache.get(tagName);
       if (cached) return cached;
 
-      const tagEffect = effect(function () {
-        const element = htmlElement(tagName);
-
-        for (let index = 0, { length } = arguments; index < length; index++) {
-          const argument = arguments[index];
-          const type = typeof argument;
-
-          if (argument instanceof Node || type === "string" || type === "number") {
-            communalSet.add(argument);
-          } else if (type === "object" && argument !== null && !Array.isArray(argument)) {
-            if (argument.__event__) {
-              htmlEventHandler(element, argument);
-            } else {
-              htmlAttributes(element, argument);
-            }
-          }
-        }
-
-        htmlChildren(element, ...communalSet);
-        communalSet.clear();
-
-        return element;
-      });
+      const tagEffect = createTagEffect(tagName);
 
       htmlCache.set(tagName, tagEffect);
       return tagEffect;
@@ -241,20 +237,57 @@ export const html = new Proxy(
   }
 );
 
-const htmlElement = effect((tagName) => document.createElement(tagName));
-const htmlChildren = function () {
-  const element = arguments[0];
+const createTagEffect = (tagName, overrideElement) =>
+  effect(
+    function () {
+      const element = overrideElement || htmlElement(tagName);
+      let hasChildren = false;
 
-  for (let index = 1, { length } = arguments; index < length; index++) {
-    const child = arguments[index];
-    const type = typeof child;
+      for (let index = 0, { length } = arguments; index < length; index++) {
+        const argument = arguments[index];
+        const type = typeof argument;
 
-    const node = type === "string" || type === "number" ? new Text(child) : child;
-    communalFragment.append(node);
-  }
+        if (argument instanceof Node || type === "string" || type === "number") {
+          communalSet.add(argument);
+          hasChildren = true;
+        } else if (type === "object" && argument !== null && !Array.isArray(argument)) {
+          if (argument.__event__) {
+            htmlEventHandler(element, argument);
+          } else {
+            htmlAttributes(element, argument);
+          }
+        }
+      }
 
-  element.replaceChildren(communalFragment);
-};
+      if (hasChildren) {
+        htmlChildren(element, ...communalSet);
+        communalSet.clear();
+      }
+
+      return element;
+    },
+    undefined,
+    tagName
+  );
+
+const htmlElement = effect((tagName) => document.createElement(tagName), undefined, "htmlElement");
+const htmlChildren = effect(
+  function () {
+    const element = arguments[0];
+
+    for (let index = 1, { length } = arguments; index < length; index++) {
+      const child = arguments[index];
+      const type = typeof child;
+
+      const node = type === "string" || type === "number" ? new Text(child) : child;
+      communalFragment.append(node);
+    }
+
+    element.replaceChildren(communalFragment);
+  },
+  undefined,
+  "htmlChildren"
+);
 const htmlAttributes = effect(
   (element, attributes) => {
     for (const key in attributes) {
@@ -285,22 +318,28 @@ const htmlAttributes = effect(
     }
 
     return a === b;
-  }
+  },
+  "htmlAttributes"
 );
-const htmlEventHandler = effect((element, { __event__: event, handler: handler, ...options }) => {
-  console.log("attaching handler", event);
-  element.addEventListener(event, handler, options);
-  onCleanup(() => {
-    console.log("removing handler", event);
-    element.removeEventListener(event, handler);
-  });
-});
+const htmlEventHandler = effect(
+  (element, { __event__: event, handler: handler, ...options }) => {
+    console.log("attaching handler", event);
+    element.addEventListener(event, handler, options);
+    onCleanup(() => {
+      console.log("removing handler", event);
+      element.removeEventListener(event, handler);
+    });
+  },
+  undefined,
+  "htmlEventHandler"
+);
 
-export const event = effect((event, handler, options) => {
-  return { __event__: event, handler, ...options };
-});
+export const event = effect(
+  (event, handler, options) => {
+    return { __event__: event, handler, ...options };
+  },
+  undefined,
+  "event"
+);
 
-export const createRoot = (element) =>
-  effect(function () {
-    htmlChildren(element, ...arguments);
-  });
+export const createRoot = (element) => createTagEffect("createdRoot", element);
