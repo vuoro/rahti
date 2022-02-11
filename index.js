8094b7d9b9fa53c201538a7b18491159db2d173b
Joni Korpi <me@jonikorpi.com>
2022-02-11 17:52:17 +0200

    Add idle

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ export * from "./effect.js";
 export * from "./state.js";
 export * from "./dom-effects.js";
 export * from "./server-side-rendering.js";
+
 export * from "./component.js";
+export * from "./idle.js";
