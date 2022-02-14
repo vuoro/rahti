c5d765a8b3b2dbbb962119286b914940a14f296b
Joni Korpi <me@jonikorpi.com>
2022-02-14 11:08:43 +0200

    Add global state

--- component.js
+++ component.js
@@ -262,3 +262,36 @@ export const state = function (initialValue, actions) {
 
   return state;
 };
+
+export const createGlobalState = (initialValue, actions) => {
+  let value = initialValue;
+  const states = new Map();
+  const cleaners = new WeakMap();
+
+  const getter = () => value;
+  const setter = (newValue) => {
+    value = newValue;
+    for (const [component, state] of states) {
+      state[0] = value;
+      update(component);
+    }
+  };
+  const finalSetter = actions ? actions(getter, setter) : setter;
+
+  const globalState = function () {
+    let state = states.get(this);
+
+    if (!state) {
+      state = [value, finalSetter];
+      states.set(this, state);
+      cleaners.set(this, (isFinal) => {
+        if (isFinal) states.delete(this);
+      });
+    }
+
+    cleanup(this).finally(cleaners.get(this));
+    return state;
+  };
+
+  return [globalState, finalSetter];
+};
